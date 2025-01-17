ca4dfeaa1cc31970076fd3e9ec3ef6aaed03bd4b
dat.lethanh2 <dat.lethanh2@ivnd.com.vn>
2025-01-17 15:46:35 +0700

    f5

--- src/main/java/org/example/ClassA.java
+++ src/main/java/org/example/ClassA.java
@@ -12,6 +12,6 @@ public class ClassA {
     public static void main(String[] args) {
         ClassA classA = new ClassA();
         ClassA.ClassB classB = classA.new ClassB();
-        System.out.println("f4-2");
+        System.out.println("f5");
     }
 }
